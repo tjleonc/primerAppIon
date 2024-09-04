2139f25fa9321c8d182b5ba4cb2e94dc5a9d69ba
tjleonc <to.leon@duocuc.cl>
2024-09-04 13:24:16 -0400

    actionsheet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,10 @@ const routes: Routes = [
     path: 'formulario',
     loadChildren: () => import('./pages/formulario/formulario.module').then( m => m.FormularioPageModule)
   },
+  {
+    path: 'actionsheet',
+    loadChildren: () => import('./pages/actionsheet/actionsheet.module').then( m => m.ActionsheetPageModule)
+  },
 ];
 
 @NgModule({
